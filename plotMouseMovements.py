1d89118278fb085f8f1446733777cf8be5d2b6e4
regi18 <31547194+regi18@users.noreply.github.com>
2019-06-07 17:46:57 +0200

    Update plotMouseMovements.py
    comments added

--- plotMouseMovements.py
+++ plotMouseMovements.py
@@ -24,6 +24,7 @@ args = parser.parse_args()
 
 f = open( args.inputfile[0], "rb" ); 
 
+# Gets the mouse informations from the file (b = button, x = x coordinate, y = y coordinate)
 def getMouseEvent():
   b, x, y = struct.unpack('3b',f.read(3))
   data.append([x + (data[len(data)-1][0]), y + (data[len(data)-1][1]), b & 0x1])
@@ -33,16 +34,21 @@ try:
   while True:
     getMouseEvent()
 finally:
+  # Enable interactive mode (matplotlib)
   plt.ion()
-
+  
+  # Iterate over the mouse data
   for a,i in enumerate(data):
+    # if left button clicked, append the data to these new lists
     if i[2] == 1:
       xList.append(i[0])
       yList.append(i[1])
+    # if the left button wasn't press, but in the cycle before it was, updates the canvas
     elif data[a-1][2] == 1:
       plt.plot(xList, yList, args.color+",")
       plt.pause(args.speed)
-
+   
+  # Disable interactive mode, and leave plot open
   plt.ioff() 
   plt.show()
   f.close()
